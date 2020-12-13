835328b0a91ec6bdec7fa8092b760d686e65637d
Chi Nguyen <phuongchi2782001@gmail.com>
2020-12-13 05:57:54 -0500

    feat: edit about page

--- chiblog/Main/main_routes.py
+++ chiblog/Main/main_routes.py
@@ -1,4 +1,7 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, request, redirect, url_for, flash
+from flask_login import login_required
+from chiblog import db
+from chiblog.Project.project_model import Project
 
 main = Blueprint("main", __name__)
 
@@ -9,8 +12,25 @@ def home():
 
 @main.route('/about')
 def about():
-    return render_template('about.html', title='About')
+    about = Project.query.filter_by(name="About").first()
+    return render_template('about.html', title='About', about=about)
 
 @main.route('/contact')
 def contact():
     return render_template('contact.html', title='Contact')
+
+@main.route('/about/edit', methods=['GET', 'POST'])
+@login_required
+def edit_about():
+    about = Project.query.filter_by(name="About").first()
+    if request.method == 'POST':
+        if about:
+            about.description = request.form["abouty"]
+            about.link = request.form["resume"]
+        else:
+            about = Project(name="About", description=request.form["abouty"], link=request.form["resume"])
+            db.session.add(about)
+        db.session.commit()
+        flash('Your about page has been updated', 'dark')
+        return redirect(url_for('main.about'))
+    return render_template('edit_about.html', title='Edit about', legend='Edit about', about=about)
