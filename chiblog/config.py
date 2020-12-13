952bff8dcde535fd5fde4270b4e6d2d6b0898efc
Chi Nguyen <phuongchi2782001@gmail.com>
2020-12-13 06:49:21 -0500

    'feat: env var'

--- chiblog/config.py
+++ chiblog/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SECRET_KEY = 'a0df23963470291ff9352a49528e043b'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     
